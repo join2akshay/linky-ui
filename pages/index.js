ee24b6d37179e33189b84b0bc82a0d8051281542
akshay <tvibhu12@gmail.com>
2020-08-01 23:34:35 +0530

    add styling

--- pages/index.js
+++ pages/index.js
@@ -73,19 +73,28 @@ console.log(e.target.value)
       </Head>
 
       <main >
-      <div className="div-one"></div>
+      {/* <div className="div-one"></div>
 
 <div className="div-two" ></div>
 
-<div className="overlay"></div>
+<div className="overlay"></div> */}
+<div className='wrap'>
+  <div className='triangle-background'></div>
+  <div className='triangle-background-weak'></div>
+  <div className='triangle-background-strong'></div>
+  <div className='github'></div>
+  <div className='footer-two'></div>
+  <div className='footer'></div>
+</div>
+
 
       <div className='box'>
 
       <h1>Linky - a free link shortner</h1>
 
       <div>
-        <input type='text' onChange={this.handleChange} />
-        <button onClick={this.handelSubmit}>
+        <input type='text' className='input-section' onChange={this.handleChange} />
+        <button className='btn-p' onClick={this.handelSubmit}>
           Short this link
         </button>
 
@@ -107,25 +116,28 @@ console.log(e.target.value)
   background:red;
   width: 100vw;
   height: 100vh;
-   background: url('http://farm1.staticflickr.com//447//19585243302_fae38fd86f_o.jpg');
+  
   width: 100vw;
   height: 100vh;
   background-repeat:no-repeat;
   background-size:cover;
   background-position:center;
   position: absolute; 
+  animation: 2s ease-out 0s 1 slideInFromLeft;
 }
 
 .div-two {
+  background:blue;
   -webkit-clip-path: polygon(100vw 0, 0% 100%, 100vw 100vh);
   clip-path: polygon(100vw 0, 0% 100vh, 100vw 100vh);
-  background: url('https://farm9.staticflickr.com/8644/15964625458_d5c6d431ac_k.jpg');
+  
   width: 100vw;
   height: 100vh;
   background-repeat:no-repeat;
   background-size:cover;
   background-position:bottom;
   position: absolute; 
+  animation: 0.5s ease-out 0s 1 slideInFromRight;
 }
 
 
@@ -154,6 +166,152 @@ console.log(e.target.value)
   padding: 0 1em; 
   font-family: Quicksand;
 }
+
+.input-section{
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px 0 0 4px;
+  box-shadow: rgba(0,0,0,.075) 0 1px 1px inset;
+  box-sizing: border-box;
+  color: #555;
+  display: table-cell;
+  float: left;
+  font-size: 14px;
+  height: 34px;
+  line-height: 1.42857;
+  margin: 0;
+  padding: 6px 12px;
+  position: relative;
+  transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+  width: 100%;
+  z-index: 2;
+}
+.btn-p{
+  -webkit-appearance: button;
+  background-color: #007bff;
+  border: 1px solid #007bff;
+  border-radius: .25rem;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  margin: 0;
+  overflow: visible;
+  padding: .375rem .75rem;
+  text-align: center;
+  text-transform: none;
+  transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+  user-select: none;
+  vertical-align: middle;
+  white-space: nowrap;
+}
+@keyframes slideInFromLeft {
+  from { opacity: 0; }
+  to { opacity: 1; }
+  }
+}
+
+@keyframes slideInFromRight {
+  from { opacity: 0; }
+  to { opacity: 1; }
+  }
+}
+
+.triangle-background {
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 700px 700px 0 0;
+  border-color: #F9F9F9 transparent transparent transparent;
+  transition: border-width 0.5s;
+}
+
+.triangle-background-weak {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 500px 500px 0 0;
+  border-color: #E8E8E8 transparent transparent transparent;
+  transition: border-width 0.5s;
+}
+
+.triangle-background-strong {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 400px 400px 0 0;
+  border-color: #D45E99 transparent transparent transparent;
+  transition: border-width 0.5s;
+}
+
+.wrap {
+  height: 100%;
+  position: relative;
+}
+.wrap:hover .triangle-background {
+  border-width: 700px 0 0 0;
+}
+.wrap:hover .triangle-background-weak {
+  border-width: 0 500px 0 0;
+}
+.wrap:hover .triangle-background-strong {
+  border-width: 400px 0 0 0;
+}
+.wrap:hover .github {
+  border-width: 0 100px 0 0;
+}
+.wrap:hover .footer {
+  border-width: 0 0 200px 200px;
+}
+
+.github {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 100px 100px 0 0;
+  border-color: black transparent transparent transparent;
+  transition: border-width 0.5s;
+}
+
+.footer {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 0 0 0 200px;
+  border-color: transparent transparent #333 transparent;
+  transition: border-width 0.5s;
+}
+
+.footer-two {
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 0 0 0 400px;
+  border-color: transparent transparent black transparent;
+  transition: border-width 0.5s;
+}
+
+
+
       `}</style>
    </div>
 
