0ee02f0e6b81fe760265270555394cb24a77178b
akshay <tvibhu12@gmail.com>
2020-08-04 22:40:50 +0530

    commit ui bugs fi:

--- pages/index.js
+++ pages/index.js
@@ -11,33 +11,45 @@ import Head from 'next/head'
 // }
 
 import React, { Component } from 'react'
+import Particles from 'react-particles-js';
+
 export default class Index extends Component {
 
   state={
    longUrl:'',
-   shortUrl:''
+   shortUrl:'',
+   disable:false,
+   Loading:false
   }
 
   handleChange=(e)=>{
-console.log(e.target.value)
+
     this.setState({
       longUrl:e.target.value
     })
 
+   
+
   }
  
   handelSubmit=()=>{
+    
+    
     const regx=/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
     if(regx.test(this.state.longUrl))
     {
+      this.setState({
+        disable:true,
+        Loading:true
+      },()=>console.log(this.state.disable))
       if(navigator.onLine){
-        fetch('http://localhost:5000/api/url/shorten', {
+        fetch('http://www.slimy.me/api/url/shorten', {
           method: 'POST', 
           headers: {
             'Content-Type': 'application/json'
           },
         body: JSON.stringify(this.state) // body data type must match "Content-Type" header
-        }).then((res)=>res.json()).then((data)=>this.setState({shortUrl:data.shortUrl})); // parses JSON response into native JavaScript objects
+        }).then((res)=>res.json()).then((data)=>this.setState({shortUrl:data.shortUrl,disable:false,Loading:false})); // parses JSON response into native JavaScript objects
       }else{
         alert('check your internet connection!!!')
     }
@@ -47,7 +59,8 @@ console.log(e.target.value)
 
     }
      
-   
+
+    
 
   }
   copyToClip=()=>{
@@ -65,7 +78,8 @@ console.log(e.target.value)
           {
             `
             body {
-  margin: 0 !important
+  margin: 0 !important;
+  background:#283145;
 }
             `
           }
@@ -73,119 +87,123 @@ console.log(e.target.value)
       </Head>
 
       <main >
-      {/* <div className="div-one"></div>
+      <Particles
+      className="headerParticles"
+      style={{position:'fixed'}}
+    params={{
+      
+	    "particles": {
+	        "number": {
+	            "value": 60,
+	            "density": {
+	                "enable": true,
+	                "value_area": 1500
+	            }
+	        },
+	        "line_linked": {
+	            "enable": true,
+	            "opacity": 0.02
+	        },
+	        "move": {
+	            "direction": "right",
+	            "speed": 0.05
+	        },
+	        "size": {
+	            "value": 1
+	        },
+	        "opacity": {
+	            "anim": {
+	                "enable": true,
+	                "speed": 1,
+	                "opacity_min": 0.05
+	            }
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onclick": {
+	                "enable": true,
+	                "mode": "push"
+	            }
+	        },
+	        "modes": {
+	            "push": {
+	                "particles_nb": 1
+	            }
+	        }
+	    },
+	    "retina_detect": true
+	}} />
+  <div style={{position:'absolute'}}>
 
-<div className="div-two" ></div>
-
-<div className="overlay"></div> */}
-<div className='wrap'>
-  <div className='triangle-background'></div>
-  <div className='triangle-background-weak'></div>
-  <div className='triangle-background-strong'></div>
-  <div className='github'></div>
-  <div className='footer-two'></div>
-  <div className='footer'></div>
+<div className="text">
+<h1>Linky - a free link shortner</h1>
 </div>
-
-
-      <div className='box'>
-
-      <h1>Linky - a free link shortner</h1>
-
-      <div>
+<div className='center-item'>
         <input type='text' className='input-section' onChange={this.handleChange} />
-        <button className='btn-p' onClick={this.handelSubmit}>
-          Short this link
+        <button className='btn-p' onClick={this.handelSubmit} disabled={this.state.disable}>
+        {
+          this.state.Loading ? 'Loading' : 'Short this link'
+        }
+          
         </button>
 
-      </div>
-      
-      {
-        this.state.shortUrl.length ? (<div onClick={this.copyToClip}  >{this.state.shortUrl}</div>):'' 
-      }
-     
-      </div>
+        {
+        this.state.shortUrl.length ? (
+        <div className='copy-box'>
 
-     
+          <div onClick={this.copyToClip}  >{this.state.shortUrl}</div>
+          
+        <span className='click-copy'>
+          Click to copy
+        </span>
+        </div>
+          ):'' 
+      }
+        </div>
+  </div>
    </main>
+
    <style jsx>{`
   
-   /* Code that does the work */
-
-.div-one {
-  background:red;
-  width: 100vw;
-  height: 100vh;
   
-  width: 100vw;
-  height: 100vh;
-  background-repeat:no-repeat;
-  background-size:cover;
-  background-position:center;
-  position: absolute; 
-  animation: 2s ease-out 0s 1 slideInFromLeft;
+
+   .text {
+	 color: #c58b8b;
+   
+    letter-spacing: .05em;
+    text-shadow: 
+      4px 4px 0px #d5d5d5, 
+      7px 7px 0px rgba(0, 0, 0, 0.2);
+	font-size:2.3em;
+	font-family:  sans-serif;
+	-webkit-animation: shift 2s ;
+  animation: shift 2s ;
+	margin-top:125px;
+	text-align:center;
+	text-transform:uppercase;
+}
+.headerParticles {
+  position: fixed;
 }
 
-.div-two {
-  background:blue;
-  -webkit-clip-path: polygon(100vw 0, 0% 100%, 100vw 100vh);
-  clip-path: polygon(100vw 0, 0% 100vh, 100vw 100vh);
-  
-  width: 100vw;
-  height: 100vh;
-  background-repeat:no-repeat;
-  background-size:cover;
-  background-position:bottom;
-  position: absolute; 
-  animation: 0.5s ease-out 0s 1 slideInFromRight;
+h2 {
+	animation: appear 3.2s;
+	text-align:center;
 }
 
-
-
-
-/* Decoration */
-
-.overlay {
-  width: 100vw;
-  height: 100vh;
-  background: rgba( 0, 0, 0, 0.3)
-  position: absolute; 
+@keyframes shift {
+	0% {opacity:0;}
+	33% {margin-top:0px;opacity:0;}
+	100% {margin-top:125px;opacity:1;text-shadow: 
+      4px 4px 0px #d5d5d5, 
+      7px 7px 0px rgba(0, 0, 0, 0.2);}
 }
 
-.box {
-  position: absolute; 
-  top: 50%; 
-  left: 50%; 
-  transform:translate(-50%, -50%); 
-  text-align: center; color: rgba(250, 250, 250, 0.8); 
-  border: 4px double rgba(250, 250, 250, 0.2)
-}
-
-.box h1 {
-  font-size: 11vh; 
-  padding: 0 1em; 
-  font-family: Quicksand;
-}
-
-.input-section{
-  background-color: #fff;
-  background-image: none;
-  border: 1px solid #ccc;
-  border-radius: 4px 0 0 4px;
-  box-shadow: rgba(0,0,0,.075) 0 1px 1px inset;
-  box-sizing: border-box;
-  color: #555;
-  display: table-cell;
-  float: left;
-  font-size: 14px;
-  height: 34px;
-  line-height: 1.42857;
-  margin: 0;
-  padding: 6px 12px;
-  position: relative;
-  transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
-  width: 100%;
-  z-index: 2;
+@keyframes appear {
+	0% {opacity:0;}
+	50% {opacity:0;}
+	100% {opacity:1;}
 }
 .btn-p{
   -webkit-appearance: button;
@@ -200,6 +218,7 @@ console.log(e.target.value)
   font-weight: 400;
   line-height: 1.5;
   margin: 0;
+  
   overflow: visible;
   padding: .375rem .75rem;
   text-align: center;
@@ -209,105 +228,46 @@ console.log(e.target.value)
   vertical-align: middle;
   white-space: nowrap;
 }
-@keyframes slideInFromLeft {
-  from { opacity: 0; }
-  to { opacity: 1; }
-  }
-}
-
-@keyframes slideInFromRight {
-  from { opacity: 0; }
-  to { opacity: 1; }
-  }
-}
-
-.triangle-background {
-  width: 0;
-  height: 0;
-  border-style: solid;
-  border-width: 700px 700px 0 0;
-  border-color: #F9F9F9 transparent transparent transparent;
-  transition: border-width 0.5s;
-}
-
-.triangle-background-weak {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 0;
-  border-style: solid;
-  border-width: 500px 500px 0 0;
-  border-color: #E8E8E8 transparent transparent transparent;
-  transition: border-width 0.5s;
-}
-
-.triangle-background-strong {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 0;
-  border-style: solid;
-  border-width: 400px 400px 0 0;
-  border-color: #D45E99 transparent transparent transparent;
-  transition: border-width 0.5s;
-}
-
-.wrap {
-  height: 100%;
+.input-section{
+  -webkit-box-flex: 1;
+  background-clip: padding-box;
+  background-color: #fff;
+  border: 1px solid #ced4da;
+  border-radius: 0 .25rem .25rem 0;
+  box-sizing: border-box;
+  color: #495057;
+  display: block;
+  flex: 1 1 auto;
+  font-size: 1rem;
+  line-height: 1.5;
+  margin: 0;
+  overflow: visible;
+  padding: .375rem .75rem;
   position: relative;
+  transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+  width: 50%;
+  margin-bottom:25px;
 }
-.wrap:hover .triangle-background {
-  border-width: 700px 0 0 0;
-}
-.wrap:hover .triangle-background-weak {
-  border-width: 0 500px 0 0;
-}
-.wrap:hover .triangle-background-strong {
-  border-width: 400px 0 0 0;
-}
-.wrap:hover .github {
-  border-width: 0 100px 0 0;
-}
-.wrap:hover .footer {
-  border-width: 0 0 200px 200px;
-}
+.center-item{
+  display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 
-.github {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 0;
-  border-style: solid;
-  border-width: 100px 100px 0 0;
-  border-color: black transparent transparent transparent;
-  transition: border-width 0.5s;
 }
-
-.footer {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  width: 0;
-  height: 0;
-  border-style: solid;
-  border-width: 0 0 0 200px;
-  border-color: transparent transparent #333 transparent;
-  transition: border-width 0.5s;
+.copy-box{
+  background: #fff;
+    padding: 5%;
+    margin: 5%;
+    border-radius: 2%;
+    font-size:20px;
+    cursor: pointer;
 }
-
-.footer-two {
-  position: absolute;
-  right: 0;
-  top: 0;
-  width: 0;
-  height: 0;
-  border-style: solid;
-  border-width: 0 0 0 400px;
-  border-color: transparent transparent black transparent;
-  transition: border-width 0.5s;
+.click-copy{
+  font-size: 15px;
+    bottom: 0;
+    position: absolute;
+    left: 45%;
 }
 
 
